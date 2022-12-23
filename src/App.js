d2576c976f3ae70deea8449279ab86e2290de874
kishoredurai <kishore.ct19@bitsathy.ac.in>
2022-12-23 18:10:50 +0530

    latest upadte

--- src/App.js
+++ src/App.js
@@ -2,45 +2,6 @@ import logo from './logo.svg'
 import './App.css'
 import { forwardRef, useState } from 'react'
 import MaterialTable from 'material-table';
-import AddBox from '@material-ui/icons/AddBox'
-import ArrowDownward from '@material-ui/icons/ArrowDownward'
-import Check from '@material-ui/icons/Check'
-import ChevronLeft from '@material-ui/icons/ChevronLeft'
-import ChevronRight from '@material-ui/icons/ChevronRight'
-import Clear from '@material-ui/icons/Clear'
-import DeleteOutline from '@material-ui/icons/DeleteOutline'
-import Edit from '@material-ui/icons/Edit'
-import FilterList from '@material-ui/icons/FilterList'
-import FirstPage from '@material-ui/icons/FirstPage'
-import LastPage from '@material-ui/icons/LastPage'
-import Remove from '@material-ui/icons/Remove'
-import SaveAlt from '@material-ui/icons/SaveAlt'
-import Search from '@material-ui/icons/Search'
-import ViewColumn from '@material-ui/icons/ViewColumn'
-
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-}
 
 
 
@@ -72,7 +33,7 @@ function App() {
   return (
     <>
      
-        <MaterialTable icons={tableIcons} 
+        <MaterialTable 
         options={{sorting:true,filtering:true,searchFieldVariant:"outlined" , exportButton:true,paging:true ,pageSize:2 ,pageSizeOptions:[2,5,10,20,30]
   , actionsColumnIndex:-1 ,addRowPosition:"first" ,selection:false ,columnsButton:true,    showFirstLastPageButtons: true,
 
@@ -80,19 +41,37 @@ function App() {
       }}
         columns={columns} data={tableData}
         actions={[
-          {icon:()=><a>delete</a>,
+          {icon:()=><form><button>delete</button></form>,
          onClick:(e,data)=>console.log(data),
         },
-          {icon:()=><a>click me</a>,
+        {icon:()=><form><button>delete</button></form>,
+         onClick:(e,data)=>console.log(data),
+        },
+        {icon:'save',
         tooltip:"click me",
        onClick:(e,data)=>console.log(data),
       }
 
         ]}
         
-        title="test table" 
+        title="" 
         
-      
+        // components={{
+        //   Action: props => (
+        //     <button
+        //       onClick={(event) => props.action.onClick(event, props.data)}
+        //       color="primary"
+        //       variant="contained"
+
+        //       style={{textTransform: 'none'}}
+        //       size="small"
+        //     >
+        //       Delete
+        //     </button>
+        //   ),
+        //   isFreeAction: true,
+        // }}
+  
         />
         
     </>
